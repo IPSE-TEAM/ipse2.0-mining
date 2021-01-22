a0ec3ed1b9ca1288415a9c09ca8c9c3c8e5728c1
wjy <1051891279@qq.com>
2021-01-22 22:39:15 +0800

    update

--- update_config.py
+++ update_config.py
@@ -60,7 +60,7 @@ def update_yaml(old_yaml, miner):
         yaml.dump(old_yaml, yaml_w)
     folder(account_id, host)
 
-    os.system("rm config.yaml")
+    # os.system("rm config.yaml")
 
 
 def get_miners_yaml():
